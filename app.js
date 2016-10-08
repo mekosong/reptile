c8c284ee7d0fae2c16bb813c46887a173c564fc8
meko <songbo.huang@szmico.com>
2016-10-08 16:28:22 +0800

    project init

---
+++ app.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var ejs = require('ejs');
+var config = require('./config');
+
+var routes = require('./server/routes/index');
+
+var app = express();
+
+
+//process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+//开放跨域请求
+app.use(function (req, res, next) {
+  res.header('content-type: application/json; charset=utf-8');
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "PUT,GET,POST,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,Cache-Control");
+
+  res.header("OPTIONSVerbHandler", "null");
+  next();
+});
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', ejs.__express);
+app.set('view engine', 'html');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+app.use(function (err, req, res, next) {
+  console.error(err);
+  res.status(err.status || 500);
+  res.send({
+    message: err.message,
+    error: err
+  });
+});
+
+
+module.exports = app;
---
+++ config/index.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Administrator on 2016/8/29.
+ */
+module.exports = {
+  workDay:[1,2,3,4,5,6,9,10,11,12,13,16]
+};
